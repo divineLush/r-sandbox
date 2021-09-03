5e58165230f48e35269ef5812ee442d39c20d7ef
divineLush <alvipath@yahoo.com>
2021-09-03 17:58:17 +0700

    control flow

---
+++ controlflow.r
@@ -0,0 +1,19 @@
+age = 18
+
+if (age >= 18) {
+	print('drink')
+} else if (age >= 16) {
+	print('hey my name is jeffrey epstein')
+} else {
+	print('loli')
+}
+
+grade = 'C'
+switch(grade,
+			 'A' = print('you suck'),
+			 'B' = print('B'),
+			 'C' = print('good job'),
+			 'D' = print('best job'),
+			 'F' = print('@_@'),
+			 print('no such grade')
+)
